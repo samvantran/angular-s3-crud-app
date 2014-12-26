f98e017ea4166706887e517b5f6b01fe090d41fe
samvantran <samvantran@gmail.com>
2014-12-26 01:45:49 -0500

    Fix: can now upload (& dl) files correctly; listing files works but doesnt autoupdate; needs promises?

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,36 +1,48 @@
-angular.module('Yellowhammer', [])
-.controller('MainCtrl', ['$scope', function($scope) {
+var app = angular.module('Yellowhammer', []);
 
-  $scope.uploadProgress = 0;
-  $scope.creds = {};
+app.controller('MainCtrl', ['$scope', function($scope, $q) {
+
+  $scope.files = [];
+  $scope.creds = {
+    access_key: '',
+    secret_key: '',
+    bucket:     'yh.interview'
+  };
 
   AWS.config.update({
-    accessKeyId:     '',
-    secretAccessKey: '',
+    accessKeyId:     $scope.creds.access_key,
+    secretAccessKey: $scope.creds.secret_key
   });
 
-  var bucket = new AWS.S3({ params:{ Bucket: 'yh.interview' }})
-
-  $scope.listBucketContents = function() {
-
-    bucket.listObjects({ Prefix: "samtran" }, function(err, data) {
-    if (err) {
-      console.log(err)
+  var s3 = new AWS.S3({ params:{ Bucket: $scope.creds.bucket }})
+  s3.listObjects({ Prefix: "samtran" }, function(err, data) {
+    if (err) { 
+      alert("Sorry, your upload was not successful. Please check the console for error details");
+      console.log(err) 
     }
     if (data) {
-      console.log(data)
-    }});
+      for(var i = 0; i < data.Contents.length; i++){
+        $scope.files.push(data.Contents[i]);
+      };
+      console.log(data);
+    }})
+
+  $scope.updateList = function() {
+    alert($scope.files.length);
+    return $scope.files;
   }
 
-  $scope.uploadFile = function() {
+  $scope.uploadFileToAWS = function() {
+    var file = document.getElementById('fileUpload').files[0];
 
-    bucket.putObject({
-      Key: "samtran/equation", // filename
-      ACL: "public-read", // must include this exactly as is, this is what allows you to get the uploaded file from your browser
-      Body: "some text or an encoded image or something" // this is the content of the file
+    s3.putObject({
+      Key: "samtran/"+file.name,
+      ACL: "public-read", // must include exactly as is, this allows you to get the uploaded file from your browser
+      Body: file 
       }, function(err, data) {
         if (err)  { console.log(err);  }
         if (data) { console.log(data); }
     })
   }
+
 }]);
