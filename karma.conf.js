caed02b8d9a9e8f6fb90302a74a8f4aad0330ae0
samvantran <samvantran@gmail.com>
2014-12-27 17:24:08 -0500

    Add: bootstrp, aws, anglr src files, now index pageload < 200ms

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'bower_components/angular/angular.min.js',
-      'bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular/angular.min.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
       'app/scripts/*.js',
       'test/**/*.js'
     ],
